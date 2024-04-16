bd548b6ef029ef6feeab9b8a3bda32368a392663
marcolaaa <marcos.ribeirorb@gmail.com>
2024-04-15 19:24:54 -0600

    Websocket chat first commit

---
+++ src/main/java/com/marcos/chat/config/WebSocketEventListener.java
@@ -0,0 +1,17 @@
+package com.marcos.chat.config;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.event.EventListener;
+import org.springframework.stereotype.Component;
+import org.springframework.web.socket.messaging.SessionDisconnectEvent;
+
+@Component
+@RequiredArgsConstructor
+@Slf4j
+public class WebSocketEventListener {
+    @EventListener
+    public void handleWebSocketDisconnectEvent(SessionDisconnectEvent event) {
+        // TODO implement the logic here.
+    }
+}
